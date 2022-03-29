eedaf75daab315286044719e64b5ab712762f521
hayeong <hayeong0624@gmail.com>
2022-03-29 20:20:50 +0900

    8일차_클래스 다이어그램

---
+++ ch3/src/exam/Animal.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class Animal {
+    
+	private String name;
+    private String kind;
+
+    public void method1(){
+    }
+
+    public int method2(){
+        return 0;
+    }
+
+}
